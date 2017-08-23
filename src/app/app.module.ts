108776b92fb87afeee0d914017825638df8ce908
ulisac19 <Isaac1904>
2017-08-23 15:26:15 -0400

    prueba

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,27 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { HttpModule } from '@angular/http';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { HolaPage } from '../pages/hola/hola';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    HolaPage
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    HolaPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,52 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { NavController, ViewController } from 'ionic-angular';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { HolaPage } from '../../pages/hola/hola';
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/map';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
 
+  datas: any;
+  params: any = [];
+  user:String;
+  pass:String;
+  app:String = "APP_WEB";
+  slideOneForm: FormGroup;
+  headers:any;
+  constructor(private viewCtrl: ViewController, public navCtrl: NavController, public formBuilder: FormBuilder, private http:Http) {
+    this.slideOneForm = formBuilder.group({
+        username: ['', Validators.compose([Validators.required, Validators.minLength(2), Validators.maxLength(25)])],
+        password: ['', Validators.compose([Validators.required, Validators.minLength(4), Validators.maxLength(25)])]
+    });
+  }
+
+  login() {
+    
+    this.params = { 'user':this.user, 'pass':this.pass, 'app':this.app};
+    this.headers = this.getHeader();
+    this.http.post("https://dev.tuten.cl/TutenREST/#!/user/login",
+    JSON.stringify(this.params)
+    , this.headers)
+        .map(res => res)
+        .subscribe(
+          data => {
+            this.datas = data.json(); 
+            console.log(this.datas);
+            
+          }			
+        );
+    this.navCtrl.push(HolaPage);   
+  }
+  getHeader(){
+    let headers = new Headers();
+    headers.append("Access-Control-Allow-Origin", "*");
+    headers.append("Access-Control-Allow-Headers", "*");
+    return headers;
   }
 
 }
